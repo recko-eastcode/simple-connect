5137375656bbd77f0809c2260d006a0ce17ec91d
Recko Jean <70039177+reckoj@users.noreply.github.com>
2022-04-25 23:25:07 -0300

    added function to display balnce of the selected account. Account is hard coded now will need to use the user account

--- src/SimpleConnect.js
+++ src/SimpleConnect.js
@@ -1,49 +1,71 @@
-import React, { useState } from 'react';
-import swal from 'sweetalert';
-import styles from './Connect.module.css';
+import React, { useState } from 'react'
+import swal from 'sweetalert'
+import styles from './Connect.module.css'
 const SimpleConnect = () => {
-  //states for the UI
-  const [connectBtnText, setConnectBtnText] = useState('Connect Wallet');
-  const [accountAddress, setAccountAddress] = useState(null);
-  const [disable, setDisable] = useState(false);
+	//states for the UI
+	const [connectBtnText, setConnectBtnText] = useState('Connect Wallet')
+	const [accountAddress, setAccountAddress] = useState(null)
+	const [accountBalance, setAccountBalance] = useState('')
 
-  const handleAccountsChanged = (newAccount) => {
-    setAccountAddress(newAccount);
-  };
+	let userAccounts
+	const handleAccountsChanged = newAccount => {
+		setAccountAddress(newAccount)
+	}
 
-  const connectWalletHandler = () => {
-    if (window.ethereum) {
-      window.ethereum
-        .request({ method: 'eth_requestAccounts' })
-        .then((result) => {
-          handleAccountsChanged(result[0]);
-          setConnectBtnText('Wallet Connected');
-          setDisable(true);
-        });
-    } else {
-      swal('MetaMask not Detected', 'Please install MetaMask', 'error');
-    }
-  };
+	const connectWalletHandler = async () => {
+		try {
+			userAccounts = await window.ethereum.request({
+				method: 'eth_requestAccounts'
+			})
 
-  return (
-    <div>
-      <h1>Simple Connect </h1>
-      <button
-        className={styles.btnClass}
-        disabled={disable}
-        onClick={() => {
-          connectWalletHandler();
-          //   setDisable(true);
-        }}
-      >
-        {connectBtnText}
-      </button>
-      <h3>
-        Account address
-        <br /> <span style={{ color: 'green' }}>{accountAddress}</span>
-      </h3>
-    </div>
-  );
-};
+			if (userAccounts) {
+				handleAccountsChanged(userAccounts[0])
+				setConnectBtnText('Wallet Connected')
+				getBalance()
+				console.log(userAccounts)
+			} else {
+				swal('MetaMask not Detected', 'Please install MetaMask', 'error')
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
-export default SimpleConnect;
+	const getBalance = async () => {
+		try {
+			let balance = await window.ethereum.request({
+				method: 'eth_getBalance',
+				//need to change from hard coded address
+				params: ['0x3870782c0594f2436e2dcb6313877d2395e36afd', 'latest']
+			})
+			let convertedBalance = parseInt(balance)
+			setAccountBalance(convertedBalance)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	return (
+		<div>
+			<h1>Simple Connect </h1>
+			<button
+				className={styles.btnClass}
+				onClick={() => {
+					connectWalletHandler()
+				}}
+			>
+				{connectBtnText}
+			</button>
+			<h3>
+				Account address
+				<br /> <span style={{ color: 'green' }}>{accountAddress}</span>
+			</h3>
+			<h3>
+				Account Balance
+				<br /> <span style={{ color: 'green' }}>{accountBalance}</span>
+			</h3>
+		</div>
+	)
+}
+
+export default SimpleConnect
